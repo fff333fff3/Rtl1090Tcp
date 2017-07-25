9c5ccdb1035230280cbae52aeec33dbfa6104258
parzivail <colby.newman2000@gmail.com>
2017-07-25 17:25:35 -0400

    Initial commit

---
+++ Rtl1090Tcp/ModeS.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rtl1090Tcp
+{
+    class ModeS
+    {
+        public static TelemetryMessage Decode(byte[] bytes)
+        {
+            using (var mem = new MemoryStream(bytes))
+            using (var br = new BinaryReader(mem))
+            {
+                var escape = br.ReadByte();
+                if (escape != 0x1A)
+                    throw new Exception();
+
+                var type = br.ReadByte();
+                
+                var time = br.ReadBytes(6);
+
+                switch (type)
+                {
+                    case 0x31:
+                        return DecodeModeAC(br);
+                    case 0x32:
+                        return DecodeShortModeS(br);
+                    case 0x33:
+                        return DecodeLongModeS(br);
+                    default:
+                        throw new NotSupportedException();
+                }
+            }
+        }
+
+        private static TelemetryMessage DecodeLongModeS(BinaryReader br)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static TelemetryMessage DecodeShortModeS(BinaryReader br)
+        {
+            var dfCa = br.ReadByte();
+            var downlinkFormat = (dfCa & 0b11111000) >> 3;
+            var capability = (dfCa & 0b00000111);
+
+            var icao = br.ReadBytes(3);
+            var aircraftAddress = (icao[0] << 16) + (icao[1] << 8) + icao[2];
+
+            return null;
+        }
+
+        private static TelemetryMessage DecodeModeAC(BinaryReader br)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class TelemetryMessage
+    {
+    }
+}
--- Rtl1090Tcp/Program.cs
+++ Rtl1090Tcp/Program.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
+using System.Net.Sockets;
 using System.Threading.Tasks;
 
 namespace Rtl1090Tcp
@@ -10,6 +11,34 @@ namespace Rtl1090Tcp
     {
         static void Main(string[] args)
         {
+            //SampleBytesAndSave();
+
+            var bytes = File.ReadAllBytes("data.bin");
+
+            var msg = bytes.Take(16).ToArray();
+
+            var decoded = ModeS.Decode(msg);
+
+            Console.ReadKey();
+        }
+
+        private static void SampleBytesAndSave()
+        {
+            var bytes = new List<byte>();
+            using (var tcp = new TcpClient("localhost", 31011))
+            using (var sr = new BinaryReader(tcp.GetStream()))
+                while (true)
+                    if (tcp.Available > 0)
+                    {
+                        var data = sr.ReadBytes(tcp.Available);
+                        var message = string.Join(" ", data.Select(b => b.ToString("X2")));
+                        bytes.AddRange(data);
+
+                        if (bytes.Count > 1000)
+                            break;
+                    }
+
+            File.WriteAllBytes("data.bin", bytes.ToArray());
         }
     }
 }
